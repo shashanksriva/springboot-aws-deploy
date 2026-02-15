d5ca0a7a2b0157f5a73171722d8337bfb88e0a5a
Srivastava <ssrivastava@luxoft.com>
2026-02-15 12:21:54 -0500

    Initial commit

---
+++ src/main/java/com/examplecode/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.examplecode.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.examplecode.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+}
